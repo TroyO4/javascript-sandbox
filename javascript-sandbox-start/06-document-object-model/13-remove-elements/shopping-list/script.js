bd7ac0522ad026d81ebba039e2e469fb737f5423
troy <oconnort@go.stockton.edu>
2025-06-02 21:40:39 -0400

    DOM JS practice

---
+++ javascript-sandbox-start/06-document-object-model/13-remove-elements/shopping-list/script.js
@@ -0,0 +1,39 @@
+function removeClearButton() {
+  clearButton = document.querySelector('#clear');
+  clearButton.remove();
+}
+
+function removeFirstItem() {
+  const ul = document.querySelector('ul');
+  const li = document.querySelector('li:first-child');
+
+  ul.removeChild(li);
+}
+
+function removeItem(itemNumber) {
+  const ul = document.querySelector('ul');
+  const li = document.querySelector(`li:nth-child(${itemNumber})`);
+
+  ul.removeChild(li);
+}
+
+function removeItem2(itemNumber) {
+  const ul = document.querySelector('ul');
+  const li = document.querySelectorAll('li')[itemNumber - 1];
+
+  ul.removeChild(li);
+}
+
+function removeItem3(itemNumber) {
+  const li = document.querySelectorAll('li');
+  li[itemNumber - 1].remove();
+}
+
+const removeItem4 = (itemNumber) =>
+  document.querySelectorAll('li')[itemNumber - 1].remove();
+
+removeClearButton();
+// removeItem(2);
+// removeItem2(2);
+// removeItem3(3);
+removeItem4(1);
